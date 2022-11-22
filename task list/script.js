6c6411a827abee5c5fdcbbfa3936a587a63f41ac
nguHelon <nguhelon@gmail.com>
2022-11-22 10:14:47 +0100

    some more javascript

--- task list/script.js
+++ task list/script.js
@@ -3,3 +3,26 @@ const nameInput = document.getElementById("name");
 const descInput = document.getElementById("desc");
 const taskContainer = document.querySelector(".container .task-container");
 const addTaskBtn = document.querySelector(".container .inputs-div .submit-btn");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+addTaskBtn.addEventListener("click", () => {
+    let nameValue = nameInput.value;
+    let descValue = descInput.value;
+    let article = document.createElement("article");
+    article.classList.add("task");
+    let content = `
+        <div class="task-info">
+            <h1>${nameValue}</h1>
+            <span>${descValue}</span>
+        </div>
+        <div class="actions">
+            <button class="completed">Completed</button>
+            <button class="delete">Delete</button>
+        </div>
+    `;
+    article.innerHTML = content;
+    taskContainer.appendChild(article);
+})
